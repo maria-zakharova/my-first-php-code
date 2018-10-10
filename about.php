08e3f6a3b905678be24a6ef37007be2237ad4166
Maria Zakharova <maria@zelandovka.ru>
2018-10-10 16:44:31 +0700

    Update about.php

---
+++ about.php
@@ -0,0 +1,21 @@
+
+ <?php 
+    $name = 'Мария';
+    $age = '38';
+    $email = 'maria@zelandovka.ru';
+    $about = 'Будущий веб-разработчик';
+
+?>
+
+<div>
+    <h1>Страница пользователя Мария</h1>
+    <p>Имя: <strong><?= $name ?></strong></p>
+    <p>Возраст: <strong><?= $age ?></strong></p>
+    <p>Email: <strong><?= $email ?></strong></p>
+    <p>О себе: <strong><?= $about ?></strong></p>
+</div>
+
+
+
+
+
